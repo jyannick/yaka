e6374f799934952353f136140aec1d4854a25d70
Yannick Jeandroz <yannick.jeandroz@gmail.com>
2021-12-25 17:22:58 +0100

    make electron app reside in the tray

--- main.js
+++ main.js
@@ -10,14 +10,29 @@ const url = require("url");
 const path = require("path");
 
 let tray; // declared outside onReady() to prevent tray icon disappearance due to garbage collection
+let quitting = false;
 
 function onReady() {
+  createMainWindow();
+  registerKeyboardShortcut();
+  setupTrayIcon();
+}
+
+function createMainWindow() {
   win = new BrowserWindow({
     width: 400,
     height: 1080,
     x: 1920 - 400,
     y: 0,
     autoHideMenuBar: true,
+    skipTaskbar: true,
+  });
+  win.on("close", function (event) {
+    if (!quitting) {
+      event.preventDefault();
+      win.hide();
+      return false;
+    }
   });
   win.loadURL(
     url.format({
@@ -26,16 +41,30 @@ function onReady() {
       slashes: true,
     })
   );
+}
+
+function registerKeyboardShortcut() {
   globalShortcut.register("Control+Alt+Y", () => {
     win.show();
   });
+}
+
+function setupTrayIcon() {
   const icon = nativeImage.createFromPath("dist/yaka/assets/tray-icon.png");
   tray = new Tray(icon);
   const contextMenu = Menu.buildFromTemplate([
-    { label: "Quit", click: () => win.close() },
+    { label: "Open", click: () => win.show() },
+    {
+      label: "Quit",
+      click: () => {
+        quitting = true;
+        win.close();
+      },
+    },
   ]);
+  tray.on("click", () => win.show());
   tray.setContextMenu(contextMenu);
-  tray.setToolTip("Yaka: no fuss todo-list manager");
+  tray.setToolTip("Yaka: the no fuss todo-list manager");
   tray.setTitle("Yaka");
 }
 
