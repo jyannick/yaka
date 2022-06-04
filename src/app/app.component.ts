a81851fb3b069d5f6f254b6052bfaf5089d45561
Yannick Jeandroz <jyannick.github@gmail.com>
2022-06-04 17:40:04 +0200

    flash background when marking done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component, HostListener, ViewEncapsulation } from '@angular/core';
-import { HELP_SCREEN } from './keyboard-shortcuts';
-import { TodoService } from './todo.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'yaka-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('flash', [
+      state('idle1', style({})),
+      state('idle2', style({})),
+      transition('idle1 <=> idle2', [style({ backgroundColor: "green" }), animate(400)])]),
+  ],
 })
 export class AppComponent {
   title = 'yaka';
   todoInput = false;
+  flashToggle = false; // flash when switching state
 
-  constructor() {}
+  constructor() { }
 
   showInput() {
     this.todoInput = true;
@@ -20,5 +26,9 @@ export class AppComponent {
 
   hideInput() {
     this.todoInput = false;
+  }
+
+  flash() {
+    this.flashToggle = !this.flashToggle;
   }
 }
